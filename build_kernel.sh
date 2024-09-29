020c1f28d3264f6b3fdc1ea016eaecde5715b5ab
RiskyGUY22 <thomas_turner@talktalk.net>
2024-09-29 14:26:22 +0100

    Modify build script to build for TARGET a21s device

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,5 +3,6 @@
 export ARCH=arm64
 export PLATFORM_VERSION=13
 
-make ARCH=arm64 exynos850-a13xx_defconfig
-make ARCH=arm64 -j16
+mkdir out
+make ARCH=arm64 exynos850-a21snsxx_defconfig O=out
+make ARCH=arm64 -j16 O=out
